1667f83fc3e289e197333fe616e668ede6f4a3e5
LiaPet <liaptsispetros@gmail.com>
2020-05-31 15:14:38 +0300

    Google login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,55 @@
 import React from 'react'
 import './styles.scss'
 import { Link } from 'react-router-dom';
+import { auth } from './../../firebase/utils';
+
 import Logo from '../../assets/logo.png';
 
 const Header = props => {
+    const { currentUser } = props;
     return (
         <header className="header">
             <div className="wrap">
                 <div className="logo">
                     <Link to="/">
-                        <img src={Logo} alt="PetLiap logo"/>
+                        <img src={Logo} alt="PetLiap logo" />
                     </Link>
                 </div>
+
                 <div className="callToActions">
-                    <ul>
-                        <li>
-                            <Link to="/registration">
-                                Register
+
+                    {currentUser && (
+                        <ul>
+                            <li>
+                                <span onClick={ () => auth.signOut()}>
+                                    LOGOUT
+                                </span>
+                            </li>
+                        </ul>
+                    )}
+
+                    {!currentUser && (
+                        <ul>
+                            <li>
+                                <Link to="/registration">
+                                    Register
                             </Link>
-                        </li>
-                    </ul>
+                            </li>
+                            <li>
+                                <Link to="/login">
+                                    Login
+                            </Link>
+                            </li>
+                        </ul>
+                    )}
                 </div>
             </div>
         </header>
     );
 };
+
+Header.defaultProps = {
+    currentUser: null
+}
 
 export default Header;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.scss';
+import Signin from './../../components/Signin';
+
+const Login = propos => {
+    return <Signin />
+}
+
+export default Login;
